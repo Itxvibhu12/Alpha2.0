4a89469e25966908247841e4c6d91210cee060e5
Itxvibhu12 <vik6026717961@gmail.com>
2022-11-23 09:57:46 +0530

    Coditional operator

---
+++ src/Conditional_solution.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Conditional_solution {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+//        /* WAP to find the number is negative or positive number */
+
+//        System.out.println("enter A number : ");
+//        int number = sc.nextInt();
+//
+//        if (number < 0){
+//        System.out.print("this number is negative ");
+//        }
+//        else {
+//            System.out.println("positive number ");
+//
+//        }
+
+
+
+
+    }
+}
