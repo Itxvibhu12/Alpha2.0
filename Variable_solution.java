d5bace7e8152e69be4420dfc038f7ef9d6ffc07f
Vikram Kumar <108029219+Itxvibhu12@users.noreply.github.com>
2022-12-14 11:29:13 +0530

    Add files via upload

---
+++ Variable_solution.java
@@ -0,0 +1,66 @@
+import java.util.*;
+
+
+public class Variable_solution {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+//       /* Find the average of 3 number */
+//        System.out.print("Enter the 1st Number: ");
+//        int Average_1st = sc.nextInt();
+//        System.out.print("Enter the 2nd Number :");
+//        int Average_2nd = sc.nextInt();
+//        System.out.print("Enter the 3rd Number :");
+//        int Average_3rd = sc.nextInt();
+//
+//        int Average = (Average_1st + Average_2nd + Average_3rd) / 3;
+//
+//        System.out.println(" Average of 3no is : " + Average);
+
+//        /* find the area of square */
+//        System.out.print("Enter the side of Square :");
+//        int side = sc.nextInt();
+//
+//        int Area = side*side;
+//        System.out.println("Area of a square : "+Area);
+
+//        /* Enter the cost of 3 items and calculate the bill */
+//
+//        System.out.print("Enter the Amount of pencil : ");
+//        float pencil = sc.nextFloat();
+//        System.out.print("Enter the Amount of Pen : ");
+//        float pen = sc.nextFloat();
+//        System.out.print("Enter the Amount of Eraser : ");
+//        float eraser = sc.nextFloat();
+//
+//        double Bill = pencil+pen+eraser;
+//        System.out.println("Total Bill : "+Bill);
+//        int N = sc.nextInt();
+//        if (N % 2 != 0){
+//            System.out.println("Weird");
+//        } else if (N % 2 == 0 || N >=2 &&  N <= 5) {
+//            System.out.println("Not Weird");
+//        } else if (N % 2 == 0 || N >= 6 && N <= 20) {
+//            System.out.println("Weird");
+//        } else if (N % 2 == 0 || N > 20) {
+//            System.out.println("Not Weird");
+//        }
+
+//        if (N % 2 != 0 ){
+//            System.out.println("Wired");
+//        }
+//        else {
+//            if (N >= 2 && N <=5){
+//                System.out.println("Not Wired");
+//            } else if (N >= 6 && N <= 20) {
+//                System.out.println("wired");
+//            } else if (N > 20 ) {
+//                System.out.println("Not Wired");
+//            }
+//        }
+
+
+
+
+    }
+}
